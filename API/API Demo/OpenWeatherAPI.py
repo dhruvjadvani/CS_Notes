ea836a5235fc75ceb2d909a9ffcfa01ee56811f0
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-07 18:17:41 +0530

    :)

--- API/API Demo/OpenWeatherAPI.py
+++ API/API Demo/OpenWeatherAPI.py
@@ -1,1 +1,14 @@
+import requests
+from pprint import pprint
+
+API_key = '9ec4a45348430b997b56f1d02eab6d57'
+
+city = input("Enter a city: ")
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + API_key + "&q=" + city
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data)
+
 
